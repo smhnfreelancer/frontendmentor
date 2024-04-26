8f34f923eaf90fd6290cc71ed87ba56491ff7b94
SMHN <smhnfreelancer@gmail.com>
2024-04-26 06:52:17 +0330

    screen font color setup

---
+++ frontendmentor/newsletter-sign-up-with-success-message-main/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        Tomato: 'hsl(4, 100%, 67%)',
+        DarkSlateGrey: 'hsl(234, 29%, 20%)',
+        CharcoalGrey: 'hsl(235, 18%, 26%)',
+        Grey: 'hsl(231, 7%, 60%)',
+        White: 'hsl(0, 0%, 100%)',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
