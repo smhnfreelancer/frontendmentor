dec19677e7a8a2cd670002c1c5e5bd980d5882f8
SMHN <smhnfreelancer@gmail.com>
2024-04-28 22:01:10 +0330

    Adding Dynamic Javascript Daily Content

--- frontendmentor/time-tracking-dashboard-main/scripts/script.js
+++ frontendmentor/time-tracking-dashboard-main/scripts/script.js
@@ -1,5 +1,5 @@
 const grid = document.querySelector('.grid');
-let dataJson;
+var dataJson;
 
 // Fetch the JSON file
 fetch('data.json')
@@ -15,6 +15,10 @@ fetch('data.json')
     // Now 'data' contains your parsed JSON data
     dataJson = data;
     console.log(data);
+    dataJson.forEach(itm => {
+      itm.toLowercase;
+      createBlock(itm);
+    });
   })
   .catch(error => {
     // Handle any errors that occurred during the fetch
@@ -23,13 +27,14 @@ fetch('data.json')
 
 // console.log(dataJson[0].timeframes.daily.current);
 
-const cardBlock = `
-<div class="work-card max-w-[327px] rounded-b-[20px] rounded-t-[15px] pt-10 md:max-w-[255px]">
+function createBlock(time) {
+  const cardBlock = `
+<div class="${time.title.toLowerCase().replace(/ /g, '_')}-card max-w-[327px] rounded-b-[20px] rounded-t-[15px] pt-10 md:max-w-[255px]">
   <div
     class="h-full rounded-[15px] rounded-b-[13px] bg-DarkBlue px-6 py-9 lg:px-7 lg:py-7"
   >
     <div class="flex flex-row justify-between">
-      <span class="text-[18px] font-normal text-white">Work</span>
+      <span class="text-[18px] font-normal text-white">${time.title}</span>
       <span class="self-center"
         ><img src="./images/icon-ellipsis.svg" alt=""
       /></span>
@@ -39,15 +44,15 @@ const cardBlock = `
     >
       <span
         class="text-[32px] font-light text-white lg:mt-6 lg:text-[56px]"
-        >${dataJson[0].timeframes.daily.current}hrs</span
+        >${time.timeframes.daily.current}hrs</span
       >
       <span
         class="self-center text-[15px] font-normal text-PaleBlue md:self-start lg:mt-2"
-        >Previous - 7hrs</span
+        >Previous - ${time.timeframes.daily.previous}hrs</span
       >
     </div>
   </div>
 </div>
 `;
-
-grid.innerHTML += cardBlock;
+  grid.innerHTML += cardBlock;
+}
