7b6885980ee84d88e2ece700ce3b166b84b11fdb
SMHN <smhnfreelancer@gmail.com>
2024-05-04 17:57:22 +0330

    HTML Code Styling

--- frontendmentor/time-tracking-dashboard-main/scripts/script.js
+++ frontendmentor/time-tracking-dashboard-main/scripts/script.js
@@ -87,26 +87,20 @@ function createDailyBlock() {
   dataJson.forEach(element => {
     const cardBlock = `
 <div class="${element.title.toLowerCase().replace(/ /g, '_')}-card max-w-[327px] rounded-b-[20px] rounded-t-[15px] pt-10 md:max-w-[255px] card">
-  <div
-    class="h-full rounded-[15px] rounded-b-[13px] bg-DarkBlue px-6 py-9 lg:px-7 lg:py-7"
-  >
+  <div class="h-full rounded-[15px] rounded-b-[13px] bg-DarkBlue px-6 py-9 lg:px-7 lg:py-7">
     <div class="flex flex-row justify-between">
       <span class="text-[18px] font-normal text-white">${element.title}</span>
-      <span class="self-center"
-        ><img src="./images/icon-ellipsis.svg" alt=""
-      /></span>
+      <span class="self-center">
+        <img src="./images/icon-ellipsis.svg" alt=""/>
+      </span>
     </div>
-    <div
-      class="mt-2 flex flex-row flex-wrap justify-between md:flex-col md:justify-start"
-    >
-      <span
-        class="text-[32px] font-light text-white lg:mt-6 lg:text-[56px]"
-        >${element.timeframes.daily.current}${element.timeframes.daily.current ? 'hrs' : ''}</span
-      >
-      <span
-        class="self-center text-[15px] font-normal text-PaleBlue md:self-start lg:mt-2"
-        >Last day - ${element.timeframes.daily.previous}${element.timeframes.daily.previous ? 'hrs' : ''}</span
-      >
+    <div class="mt-2 flex flex-row flex-wrap justify-between md:flex-col md:justify-start">
+      <span class="text-[32px] font-light text-white lg:mt-6 lg:text-[56px]" >
+      ${element.timeframes.daily.current}${element.timeframes.daily.current ? 'hrs' : ''}
+      </span>
+      <span class="self-center text-[15px] font-normal text-PaleBlue md:self-start lg:mt-2">
+        Last day - ${element.timeframes.daily.previous}${element.timeframes.daily.previous ? 'hrs' : ''}
+        </span>
     </div>
   </div>
 </div>
@@ -121,26 +115,22 @@ function createWeeklyBlock() {
   dataJson.forEach(element => {
     const cardBlock = `
 <div class="${element.title.toLowerCase().replace(/ /g, '_')}-card max-w-[327px] rounded-b-[20px] rounded-t-[15px] pt-10 md:max-w-[255px] card">
-  <div
-    class="h-full rounded-[15px] rounded-b-[13px] bg-DarkBlue px-6 py-9 lg:px-7 lg:py-7"
-  >
+  <div class="h-full rounded-[15px] rounded-b-[13px] bg-DarkBlue px-6 py-9 lg:px-7 lg:py-7">
     <div class="flex flex-row justify-between">
-      <span class="text-[18px] font-normal text-white">${element.title}</span>
-      <span class="self-center"
-        ><img src="./images/icon-ellipsis.svg" alt=""
-      /></span>
+      <span class="text-[18px] font-normal text-white">
+        ${element.title}
+      </span>
+      <span class="self-center">
+        <img src="./images/icon-ellipsis.svg" alt=""/>
+      </span>
     </div>
-    <div
-      class="mt-2 flex flex-row flex-wrap justify-between md:flex-col md:justify-start"
-    >
-      <span
-        class="text-[32px] font-light text-white lg:mt-6 lg:text-[56px]"
-        >${element.timeframes.weekly.current}${element.timeframes.weekly.current ? 'hrs' : ''}</span
-      >
-      <span
-        class="self-center text-[15px] font-normal text-PaleBlue md:self-start lg:mt-2"
-        >Last week - ${element.timeframes.weekly.previous}${element.timeframes.weekly.previous ? 'hrs' : ''}</span
-      >
+    <div class="mt-2 flex flex-row flex-wrap justify-between md:flex-col md:justify-start">
+      <span class="text-[32px] font-light text-white lg:mt-6 lg:text-[56px]">
+        ${element.timeframes.weekly.current}${element.timeframes.weekly.current ? 'hrs' : ''}
+      </span>
+      <span class="self-center text-[15px] font-normal text-PaleBlue md:self-start lg:mt-2">
+        Last week - ${element.timeframes.weekly.previous}${element.timeframes.weekly.previous ? 'hrs' : ''}
+      </span>
     </div>
   </div>
 </div>
@@ -155,26 +145,22 @@ function createMonthlyBlock() {
   dataJson.forEach(element => {
     const cardBlock = `
 <div class="${element.title.toLowerCase().replace(/ /g, '_')}-card max-w-[327px] rounded-b-[20px] rounded-t-[15px] pt-10 md:max-w-[255px] card">
-  <div
-    class="h-full rounded-[15px] rounded-b-[13px] bg-DarkBlue px-6 py-9 lg:px-7 lg:py-7"
-  >
+  <div class="h-full rounded-[15px] rounded-b-[13px] bg-DarkBlue px-6 py-9 lg:px-7 lg:py-7">
     <div class="flex flex-row justify-between">
-      <span class="text-[18px] font-normal text-white">${element.title}</span>
-      <span class="self-center"
-        ><img src="./images/icon-ellipsis.svg" alt=""
-      /></span>
+      <span class="text-[18px] font-normal text-white">
+        ${element.title}
+      </span>
+      <span class="self-center">
+        <img src="./images/icon-ellipsis.svg" alt=""/>
+      </span>
     </div>
-    <div
-      class="mt-2 flex flex-row flex-wrap justify-between md:flex-col md:justify-start"
-    >
-      <span
-        class="text-[32px] font-light text-white lg:mt-6 lg:text-[56px]"
-        >${element.timeframes.monthly.current}${element.timeframes.monthly.current ? 'hrs' : ''}</span
-      >
-      <span
-        class="self-center text-[15px] font-normal text-PaleBlue md:self-start lg:mt-2"
-        >Last month - ${element.timeframes.monthly.previous}${element.timeframes.monthly.previous ? 'hrs' : ''}</span
-      >
+    <div class="mt-2 flex flex-row flex-wrap justify-between md:flex-col md:justify-start">
+      <span class="text-[32px] font-light text-white lg:mt-6 lg:text-[56px]">
+        ${element.timeframes.monthly.current}${element.timeframes.monthly.current ? 'hrs' : ''}
+      </span>
+      <span class="self-center text-[15px] font-normal text-PaleBlue md:self-start lg:mt-2">
+        Last month - ${element.timeframes.monthly.previous}${element.timeframes.monthly.previous ? 'hrs' : ''}
+      </span>
     </div>
   </div>
 </div>
