79f09045e89f56b6b2a3d14e0e86316c897a3050
SMHN <smhnfreelancer@gmail.com>
2024-05-05 16:48:32 +0330

    Code Refactoring

--- frontendmentor/time-tracking-dashboard-main/scripts/script.js
+++ frontendmentor/time-tracking-dashboard-main/scripts/script.js
@@ -3,65 +3,28 @@ const subgrid = document.querySelector('.grid-cols-subgrid');
 const daily = document.getElementById('daily');
 const weekly = document.getElementById('weekly');
 const monthly = document.getElementById('monthly');
-const cards = subgrid.querySelectorAll('.card');
 const LI = document.querySelectorAll('li');
 
 var dataJson;
 
 function dailyLoading() {
   removeActiveLink();
-  fetchData().then(data => {
-    if (data !== null) {
-      dataJson = data;
-      console.log(data); // Output the fetched data
-      // Now you can use the 'data' variable to access the fetched data
-    } else {
-      console.log('Data could not be fetched');
-    }
-  });
-  removeCards();
   createDailyBlock();
 }
 
 function weeklyLoading() {
   removeActiveLink();
-  fetchData().then(data => {
-    if (data !== null) {
-      dataJson = data;
-      console.log(data); // Output the fetched data
-      // Now you can use the 'data' variable to access the fetched data
-    } else {
-      console.log('Data could not be fetched');
-    }
-  });
-  removeCards();
   createWeeklyBlock();
 }
 
 function monthlyLoading() {
   removeActiveLink();
-  fetchData().then(data => {
-    if (data !== null) {
-      dataJson = data;
-      console.log(data); // Output the fetched data
-      // Now you can use the 'data' variable to access the fetched data
-    } else {
-      console.log('Data could not be fetched');
-    }
-  });
-  removeCards();
   createMonthlyBlock();
 }
 
 function removeActiveLink() {
   LI.forEach(li => {
     li.classList.remove('text-white');
-  });
-}
-
-function removeCards() {
-  cards.forEach(card => {
-    card.remove();
   });
 }
 
@@ -85,104 +48,110 @@ function fetchData() {
 function createDailyBlock() {
   daily.classList.add('text-white');
   subgrid.innerHTML = '';
-  console.log(dataJson);
-  dataJson.forEach(element => {
-    const cardBlock = `
-<div class="${element.title.toLowerCase().replace(/ /g, '_')}-card max-w-[327px] rounded-b-[20px] rounded-t-[15px] pt-10 md:max-w-[255px] card">
-  <div class="h-full rounded-[15px] rounded-b-[13px] bg-DarkBlue px-6 py-9 lg:px-7 lg:py-7">
-    <div class="flex flex-row justify-between">
-      <span class="text-[18px] font-normal text-white">${element.title}</span>
-      <span class="self-center">
-        <img src="./images/icon-ellipsis.svg" alt=""/>
-      </span>
-    </div>
-    <div class="mt-2 flex flex-row flex-wrap justify-between md:flex-col md:justify-start">
-      <span class="text-[32px] font-light text-white lg:mt-6 lg:text-[56px]" >
-      ${element.timeframes.daily.current}${element.timeframes.daily.current ? 'hrs' : ''}
-      </span>
-      <span class="self-center text-[15px] font-normal text-PaleBlue md:self-start lg:mt-2">
-        Last day - ${element.timeframes.daily.previous}${element.timeframes.daily.previous ? 'hrs' : ''}
-        </span>
-    </div>
-  </div>
-</div>
+  fetchData().then(data => {
+    if (data !== null) {
+      data.forEach(element => {
+        const cardBlock = `
+                <div class="${element.title.toLowerCase().replace(/ /g, '_')}-card max-w-[327px] rounded-b-[20px] rounded-t-[15px] pt-10 md:max-w-[255px] card">
+                  <div class="h-full rounded-[15px] rounded-b-[13px] bg-DarkBlue px-6 py-9 lg:px-7 lg:py-7">
+                    <div class="flex flex-row justify-between">
+                      <span class="text-[18px] font-normal text-white">${element.title}</span>
+                      <span class="self-center">
+                        <img src="./images/icon-ellipsis.svg" alt=""/>
+                      </span>
+                    </div>
+                    <div class="mt-2 flex flex-row flex-wrap justify-between md:flex-col md:justify-start">
+                      <span class="text-[32px] font-light text-white lg:mt-6 lg:text-[56px]" >
+                      ${element.timeframes.daily.current}${element.timeframes.daily.current ? 'hrs' : ''}
+                      </span>
+                      <span class="self-center text-[15px] font-normal text-PaleBlue md:self-start lg:mt-2">
+                        Last day - ${element.timeframes.daily.previous}${element.timeframes.daily.previous ? 'hrs' : ''}
+                        </span>
+                    </div>
+                  </div>
+                </div>
 `;
-    subgrid.innerHTML += cardBlock;
+        subgrid.innerHTML += cardBlock;
+      });
+    } else {
+      console.log('Data could not be fetched');
+    }
   });
 }
 
 function createWeeklyBlock() {
   weekly.classList.add('text-white');
   subgrid.innerHTML = '';
-  console.log(dataJson);
-  dataJson.forEach(element => {
-    const cardBlock = `
-<div class="${element.title.toLowerCase().replace(/ /g, '_')}-card max-w-[327px] rounded-b-[20px] rounded-t-[15px] pt-10 md:max-w-[255px] card">
-  <div class="h-full rounded-[15px] rounded-b-[13px] bg-DarkBlue px-6 py-9 lg:px-7 lg:py-7">
-    <div class="flex flex-row justify-between">
-      <span class="text-[18px] font-normal text-white">
-        ${element.title}
-      </span>
-      <span class="self-center">
-        <img src="./images/icon-ellipsis.svg" alt=""/>
-      </span>
-    </div>
-    <div class="mt-2 flex flex-row flex-wrap justify-between md:flex-col md:justify-start">
-      <span class="text-[32px] font-light text-white lg:mt-6 lg:text-[56px]">
-        ${element.timeframes.weekly.current}${element.timeframes.weekly.current ? 'hrs' : ''}
-      </span>
-      <span class="self-center text-[15px] font-normal text-PaleBlue md:self-start lg:mt-2">
-        Last week - ${element.timeframes.weekly.previous}${element.timeframes.weekly.previous ? 'hrs' : ''}
-      </span>
-    </div>
-  </div>
-</div>
+  fetchData().then(data => {
+    if (data !== null) {
+      data.forEach(element => {
+        const cardBlock = `
+                <div class="${element.title.toLowerCase().replace(/ /g, '_')}-card max-w-[327px] rounded-b-[20px] rounded-t-[15px] pt-10 md:max-w-[255px] card">
+                  <div class="h-full rounded-[15px] rounded-b-[13px] bg-DarkBlue px-6 py-9 lg:px-7 lg:py-7">
+                    <div class="flex flex-row justify-between">
+                      <span class="text-[18px] font-normal text-white">
+                        ${element.title}
+                      </span>
+                      <span class="self-center">
+                        <img src="./images/icon-ellipsis.svg" alt=""/>
+                      </span>
+                    </div>
+                    <div class="mt-2 flex flex-row flex-wrap justify-between md:flex-col md:justify-start">
+                      <span class="text-[32px] font-light text-white lg:mt-6 lg:text-[56px]">
+                        ${element.timeframes.weekly.current}${element.timeframes.weekly.current ? 'hrs' : ''}
+                      </span>
+                      <span class="self-center text-[15px] font-normal text-PaleBlue md:self-start lg:mt-2">
+                        Last week - ${element.timeframes.weekly.previous}${element.timeframes.weekly.previous ? 'hrs' : ''}
+                      </span>
+                    </div>
+                  </div>
+                </div>
 `;
-    subgrid.innerHTML += cardBlock;
+        subgrid.innerHTML += cardBlock;
+      });
+    } else {
+      console.log('Data could not be fetched');
+    }
   });
 }
 
 function createMonthlyBlock() {
   monthly.classList.add('text-white');
   subgrid.innerHTML = '';
-  console.log(dataJson);
-  dataJson.forEach(element => {
-    const cardBlock = `
-<div class="${element.title.toLowerCase().replace(/ /g, '_')}-card max-w-[327px] rounded-b-[20px] rounded-t-[15px] pt-10 md:max-w-[255px] card">
-  <div class="h-full rounded-[15px] rounded-b-[13px] bg-DarkBlue px-6 py-9 lg:px-7 lg:py-7">
-    <div class="flex flex-row justify-between">
-      <span class="text-[18px] font-normal text-white">
-        ${element.title}
-      </span>
-      <span class="self-center">
-        <img src="./images/icon-ellipsis.svg" alt=""/>
-      </span>
-    </div>
-    <div class="mt-2 flex flex-row flex-wrap justify-between md:flex-col md:justify-start">
-      <span class="text-[32px] font-light text-white lg:mt-6 lg:text-[56px]">
-        ${element.timeframes.monthly.current}${element.timeframes.monthly.current ? 'hrs' : ''}
-      </span>
-      <span class="self-center text-[15px] font-normal text-PaleBlue md:self-start lg:mt-2">
-        Last month - ${element.timeframes.monthly.previous}${element.timeframes.monthly.previous ? 'hrs' : ''}
-      </span>
-    </div>
-  </div>
-</div>
-`;
-    subgrid.innerHTML += cardBlock;
-  });
-}
-
-function init() {
   fetchData().then(data => {
     if (data !== null) {
-      dataJson = data;
-      console.log(data); // Output the fetched data
-      // Now you can use the 'data' variable to access the fetched data
+      data.forEach(element => {
+        const cardBlock = `
+                <div class="${element.title.toLowerCase().replace(/ /g, '_')}-card max-w-[327px] rounded-b-[20px] rounded-t-[15px] pt-10 md:max-w-[255px] card">
+                  <div class="h-full rounded-[15px] rounded-b-[13px] bg-DarkBlue px-6 py-9 lg:px-7 lg:py-7">
+                    <div class="flex flex-row justify-between">
+                      <span class="text-[18px] font-normal text-white">
+                        ${element.title}
+                      </span>
+                      <span class="self-center">
+                        <img src="./images/icon-ellipsis.svg" alt=""/>
+                      </span>
+                    </div>
+                    <div class="mt-2 flex flex-row flex-wrap justify-between md:flex-col md:justify-start">
+                      <span class="text-[32px] font-light text-white lg:mt-6 lg:text-[56px]">
+                        ${element.timeframes.monthly.current}${element.timeframes.monthly.current ? 'hrs' : ''}
+                      </span>
+                      <span class="self-center text-[15px] font-normal text-PaleBlue md:self-start lg:mt-2">
+                        Last month - ${element.timeframes.monthly.previous}${element.timeframes.monthly.previous ? 'hrs' : ''}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+`;
+        subgrid.innerHTML += cardBlock;
+      });
     } else {
       console.log('Data could not be fetched');
     }
   });
+}
+
+function init() {
   daily.addEventListener('click', dailyLoading);
   weekly.addEventListener('click', weeklyLoading);
   monthly.addEventListener('click', monthlyLoading);
